d2d4922e7ba004208bd265d0524642af5709fe9a
Alec Johnson <akj3211@gmail.com>
2021-12-08 17:11:19 -0600

    Finished auth, setup Apollo provider and the refresh token functionality

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { GlobalStyles } from './components/GlobalStyles';
+import { Layout } from './components/Layout';
+import { Loading } from "./components/Loading";
+import { isAuthenticated, usePrepareApp } from "./helper/auth";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+
 function App() {
+  const { isLoading } = usePrepareApp();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <GlobalStyles />
+      <Layout>
+        <Routes>
+          <Route path='/' element={<RequireAuth><Home /></RequireAuth>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
 
+const RequireAuth: React.FC<{children: JSX.Element}> = ({ children }) => {
+  const auth = isAuthenticated(); // determine if authorized by checking local storage for token
+
+  // If authorized, return and render the child elements
+  // If not, return element that will navigate to login page
+  return auth ? children : <Navigate to="/login" replace />;
+}
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloClient, ApolloProvider, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { getToken, isAuthenticated, saveToken } from "./helper/auth";
+import reportWebVitals from "./reportWebVitals";
 
-const httpLink = createHttpLink({uri: "http://localhost:4000/graphql", credentials: "include"});
+export const EXPRESS_URL = process.env.REACT_APP_EXPRESS_URL;
+export const GRAPHQL_URL = `${EXPRESS_URL}/graphql`;
+
+const refreshLink = new TokenRefreshLink({
+  accessTokenField: "access_token",
+  isTokenValidOrUndefined: () => isAuthenticated(),
+  fetchAccessToken: () => {
+    return fetch(`${EXPRESS_URL}/refresh-token`, {
+      method: "POST",
+      credentials: "include",
+    });
+  },
+  handleFetch: (accessToken) => saveToken(accessToken),
+  handleError: (err) => {
+    console.warn("Your refresh token is invalid. Try to relogin");
+    console.error(err);
+  },
+});
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_URL,
+  credentials: "include",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = getToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: ApolloLink.from([httpLink]),
-  cache: new InMemoryCache()
-})
+  link: ApolloLink.from([refreshLink, authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +59,7 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
