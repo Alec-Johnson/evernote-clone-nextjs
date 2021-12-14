be41ad057874716eea30d5a92ce387e7d28c8778
Alec Johnson <akj3211@gmail.com>
2021-12-14 15:35:58 -0600

    Dockerize application

--- server/src/constants/strings.ts
+++ server/src/constants/strings.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 
 export const CONST = {
+	MYSQL_HOST: process.env.MYSQL_HOST || "localhost",
+	REACT_WEB: process.env.REACT_WEB,
 	PORT: process.env.PORT!,
 	ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET!,
 	REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET!,
